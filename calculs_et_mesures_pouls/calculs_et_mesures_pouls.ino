604cce0c9eb9f710a6599e3845f687a86a22c7cd
TristanBoulesteix <33152826+TristanBoulesteix@users.noreply.github.com>
2017-11-13 13:43:48 +0100

    remplissage de main.ino

--- calculs_et_mesures_pouls/calculs_et_mesures_pouls.ino
+++ calculs_et_mesures_pouls/calculs_et_mesures_pouls.ino
@@ -1,9 +1,12 @@
+//const int ledInfrarouge = 8; //Numéro de la broche qui contrôle la LED infrarouge
+//int temps = 0; //Initialisation du temps
+
 void setup() {
   // put your setup code here, to run once:
-  
+  //digitalWrite(ledInfrarouge, high);//Met la LED infrarouge à l'état haut (allumage de la LED)
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  //analogRead(A0);
 }
