735f7e94f7011b9a484682f6e551eb12ca91ef60
Luca BORRUTO <33548688+lborruto@users.noreply.github.com>
2017-11-13 14:25:58 +0100

    a

--- calculs_et_mesures_pouls/calculs_et_mesures_pouls.ino
+++ calculs_et_mesures_pouls/calculs_et_mesures_pouls.ino
@@ -2,15 +2,15 @@
 
 const int ledInfrarouge = 8; //Numéro de la broche qui contrôle la LED infrarouge
 unsigned long temps = 0; //Initialisation du temps
+float lecture;//pour afficher le résultat 
 
 void setup() {
   // put your setup code here, to run once:
-  float lecture;//pour afficher le résultat 
-  digitalWrite(ledInfrarouge, HIGH);//Met la LED infrarouge à l'état haut (allumage de la LED)
+  digitalWrite(ledInfrarouge, HIGH); //Met la LED infrarouge à l'état haut (allumage de la LED)
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   temps = millis();//enregistrement dans "temps" de la durée d'exécution du programme en millisecondes
-  lecture = analogRead(0);//Lecture de l'état du phototransistor dans la variable "lecture"
+  lecture = analogRead(0); //Lecture de l'état du phototransistor dans la variable "lecture"
 }
