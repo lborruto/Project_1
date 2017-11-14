d728a6d33367757ef3127c16c8c2ff44acab13af
TristanBoulesteix <33152826+TristanBoulesteix@users.noreply.github.com>
2017-11-14 11:07:24 +0100

    trying something...

--- module_arduino_pouls/cardio.h
+++ module_arduino_pouls/cardio.h
@@ -1,6 +1,14 @@
 //Ce fichier contient toutes les fonctions nécessaires au calcul du pouls.
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int valeurPouls(int lecture, unsigned long temps);
 /*fonction calculPouls
  * calcule le pouls en fonction du nombre de battements
  */
+
+ #ifdef __cplusplus
+extern "C" {
+#endif
