6bf33e9d1d2165d472d96f81f1db415885cf5ec1
Luca BORRUTO <33548688+lborruto@users.noreply.github.com>
2017-11-17 10:45:16 +0100

    last update

--- module_2/code/code_final/menu.cpp
+++ module_2/code/code_final/menu.cpp
@@ -10,7 +10,6 @@ int menu()
 		printf("1 - Allumer toutes les LEDs \n");
 		printf("2 - Allumer une LED sur deux \n");
 		printf("3 - Allumer une LED au hasard \n");
-		printf("4 - Allumer les LEDs en mode chenille à la vitesse des battements de coeur \n");
 		scanf("%d", &choix);
 
 		if (choix<1 || choix>4)
